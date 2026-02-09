0de330779f5f525afe5e59ed6ef1d097b41ee34c
jordangarrison <jordangarrison@users.noreply.github.com>
2026-02-09 00:44:40 +0000

    chore: update warp-terminal-preview to 0.2026.02.04.08.20.preview.03

--- flake.nix
+++ flake.nix
@@ -14,11 +14,11 @@
     packages = forAll (pkgs:
       let
         debUrl = "https://app.warp.dev/download?channel=preview&package=deb";
-        debSha = "sha256-9BV8FFICofCo5Sc6ovdPzK25pbs6pYaTJBzdN8JPOz4=";
+        debSha = "sha256-PjQh0OLi4lrUxhLHLMNihLUrLGrogmcbE55shKP2wi8=";
       in {
         default = pkgs.stdenv.mkDerivation {
           pname   = "warp-terminal-preview";
-          version = "0.2026.01.07.08.13.preview.01";
+          version = "0.2026.02.04.08.20.preview.03";
 
           src = pkgs.fetchurl {
             url = debUrl;
